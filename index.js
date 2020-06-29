b826dbf09a2259a22553cd823b76995f86a73f84
agilan2001 <63188339+agilan2001@users.noreply.github.com>
2020-06-29 19:09:42 +0530

    privacy commit

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const { Client } = require('whatsapp-web.js');
 var MessageMedia = require("whatsapp-web.js/src/structures/MessageMedia.js");
 var msg_data = { hist: [] };
 
+var my_num = "<YOUR MOBILE NUMBER>";
+
 var client = new Client({
-    session: { "WABrowserId": "\"F6Cb0xz0utNsv2uI87mH0A==\"", "WASecretBundle": "{\"key\":\"RMKtKypKtQWHfkfDah9/KaHGUHkDQvf7M55I5o0UYh0=\",\"encKey\":\"J/m31CUxLvxfCZ0MX526w28HfKFK+EXIJZzEhecyfsw=\",\"macKey\":\"RMKtKypKtQWHfkfDah9/KaHGUHkDQvf7M55I5o0UYh0=\"}", "WAToken1": "\"7a9T6ZnTQFmFGzB023UsqPMJM8/VBpjemhwpcpvMkQI=\"", "WAToken2": "\"1@QgY0+505U34Y49ajGPjCZqDEcDsKcoXqfob+bs6MXs2qMvemm1gDTp3UkOK9gdWezcb5ynb/nzNdIg==\"" },
+    //session: { Your SESSION DATA },
     puppeteer: {
         args: [
             '--no-sandbox',
@@ -17,7 +19,7 @@ var client = new Client({
 
 client.on('ready', () => {
     console.log('Client is ready!');
-    client.sendMessage("919384495336@c.us", 'Client is ready!');
+    client.sendMessage(my_num + "@c.us", 'Client is ready!');
 
     http.createServer((req, res) => {
         cors(req, res, function () {
@@ -26,14 +28,7 @@ client.on('ready', () => {
 
                 return;
             }
-            if (url.parse(req.url, true).pathname == "/reset_for_me") {
-                msg_data["918098255246"] = 0;
-                msg_data["919384495336"] = 0;
-
-                res.end("reset");
-
-                return;
-            }
+            
             var qry = url.parse(req.url, true).query;
             if (qry.num) {
                 if (msg_data[qry.num] == undefined) msg_data[qry.num] = 0;
